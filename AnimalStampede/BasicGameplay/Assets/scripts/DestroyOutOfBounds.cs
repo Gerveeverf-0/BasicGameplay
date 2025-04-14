ecca1676da96267755b644eb29c9eeb7a8197f78
Gerveeverf <wolforda@roverkids.org>
2025-04-14 14:18:28 -0400

    lesson 2.4 complete

--- AnimalStampede/BasicGameplay/Assets/scripts/DestroyOutOfBounds.cs
+++ AnimalStampede/BasicGameplay/Assets/scripts/DestroyOutOfBounds.cs
@@ -19,5 +19,6 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound) { Destroy(gameObject); } }
+        } else if (transform.position.z < lowerBound) { Debug.Log("Game Over!");
+            Destroy(gameObject); } }
     }
